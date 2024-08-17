5e3ceac7708fc8a0db5b75ef8eaa79f6fde52dfd
bbaa <bbaa@bbaa.fun>
2024-08-17 21:45:20 +0800

    optimize resources

--- assets/workbox-config.js
+++ assets/workbox-config.js
@@ -1,13 +1,19 @@
 module.exports = {
 	globDirectory: '.',
 	globPatterns: [
-		'**/*.{html,otf,woff2,woff,md,js,css,png,jpg,ico,ijmap}',
-                'manifest.json'
+		'**/*.{js,css,ico}',
+    'manifest.json',
+    'static/mdui/fonts/roboto/{Roboto-Regular.woff2,Roboto-Medium.woff2,Roboto-Bold.woff2}',
+    'static/mdui/icons/material-icons/MaterialIcons-Regular.woff2',
+    'static/font/Bender/Bender-Bold.woff2',
+    'static/font/Hack/hack-regular.woff2',
 	],
+  /*
   globIgnores:[
     'index.html',
     'admin.html'
   ],
+  */
   skipWaiting: true,
   clientsClaim: true,
 	swDest: 'sw.js',
@@ -20,7 +26,7 @@ module.exports = {
     {
       urlPattern: function(options){
         const url = options.url;
-        return url.pathname.startsWith('/static/') && (url.pathname.endsWith('.js') || url.pathname.endsWith('.css'));
+        return url.pathname.startsWith('/static/img/') || url.pathname.startsWith('/static/') && (url.pathname.endsWith('.js') || url.pathname.endsWith('.css') || url.pathname.endsWith('.woff') || url.pathname.endsWith('.woff2'));
       },
       handler: 'StaleWhileRevalidate'
     },
