bfe3120b68ad6ff08b2e742662f257c3cb0129a7
bbaa <bbaa@bbaa.fun>
2024-09-12 20:19:53 +0800

    fix

--- assets/static/js/admin.js
+++ assets/static/js/admin.js
@@ -207,9 +207,9 @@
         }
         pastes_panel += `</div>`;
         if (paste.filename != "" && paste.filename != "-") {
-          pastes_panel += `<div class="mdui-panel-item-title" style="overflow: visible;">${paste.filename}</div>`;
+          pastes_panel += `<div class="mdui-panel-item-title" style="width: max-content;">${paste.filename}</div>`;
         } else {
-          pastes_panel += `<div class="mdui-panel-item-title" style="overflow: visible;">${paste.short_url || paste.hash}</div>`;
+          pastes_panel += `<div class="mdui-panel-item-title" style="width: max-content;">${paste.short_url || paste.hash}</div>`;
         }
         if (paste.user) {
           pastes_panel += `
@@ -217,7 +217,7 @@
         `;
         }
         pastes_panel += `
-          <div class="mdui-panel-item-summary mdui-invisible-xs-down">Time: ${paste.created_at.substring(0, Math.min(23, paste.created_at.length))}</div>
+          <div class="mdui-panel-item-summary mdui-invisible-sm-down">Time: ${paste.created_at.substring(0, Math.min(23, paste.created_at.length))}</div>
         `;
         pastes_panel += `
               <i class="mdui-panel-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
