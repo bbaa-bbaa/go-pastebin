5b158fdfe46f0a7d7a2c13674586d956e4c74327
bbaa <bbaa@bbaa.fun>
2025-03-18 21:24:16 +0800

    fix url

--- assets/static/js/pastebin.js
+++ assets/static/js/pastebin.js
@@ -220,7 +220,7 @@
     };
 
     function testVaildURL(url) {
-      return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/im.test(url);
+      return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/i.test(url);
     }
 
     (function new_paste() {
